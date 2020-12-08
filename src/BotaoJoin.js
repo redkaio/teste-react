1d65ada191d2f01ce3bd7420612ff67d8ad274ea
Kaio C <chrno.92@gmail.com>
2020-12-08 17:01:45 -0300

    esqueleto do projeto pronto

---
+++ src/BotaoJoin.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './style.css';
+
+
+const BotaoJoin = () => {
+
+
+
+    return (
+  
+  
+  
+  
+        <Button variant="primary" style={{marginBottom: '10px'}}>Participar</Button>
+      
+  
+  
+  
+  
+  
+    );
+  };
+  
+  
+  
+  
+  export default BotaoJoin;
--- src/CardMoreTest.js
+++ src/CardMoreTest.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import { Grid } from '@material-ui/core';
 import './style.css';
+import BotaoJoin from './BotaoJoin.js';
 
 
 
@@ -20,12 +21,18 @@ const CardMoreTest = props => {
     <Container>
       <Grid container spacing={4}>
         <Grid item xs={12} sm={6} md={4} lg={4} xlg={4}></Grid>
-        <Card style={{ width: '300px', height: '25rem', marginTop:'3px', marginBottom:'50px' }}>
-        <Card style={{ width: '100%', height: '100%' }}>
-            <div className='card-header'>{ props.cardHeader }</div>
-            <div className='card-descricao'>{props.cardDescricao}</div>
+        <Card style={{ width: '300px', height: '25rem', marginTop: '3px', marginBottom: '50px' }}>
+          <Card style={{ width: '100%', height: '100%' }}>
+            <div className='card-header'>{props.cardHeader}</div>
+            <div className='split'>
+              <div className='f-half'>{props.cardDescricao}</div>
+              <div className='l-half'>Icone</div>
+            </div>
             <div className='card-sp'>{props.cardSp}</div>
-            <div className='card-join'>{props.cardJoin}</div>
+            <div className='split'>
+              <div className='f-half'>{props.cardJoin}</div>
+              <div className='l-half'><BotaoJoin /></div>
+            </div>
             <div className='card-footer'>{props.cardLocal}</div>
 
           </Card>
